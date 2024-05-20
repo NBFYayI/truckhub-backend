c5988e9d17e2d4eda440f6341fb8118c98d711d6
Yoshisora <93548125+Yoshisora@users.noreply.github.com>
2024-05-19 20:57:30 -0700

    fk

--- src/controllers/post-control.js
+++ src/controllers/post-control.js
@@ -42,8 +42,8 @@ async function searchPost(
 
     //if (id) filter._id = id;
     if (author) filter.author = author;
-    if (title) filter.title = { title: new RegExp("^" + title, "i") };
-    if (content) filter.content = { title: new RegExp(content, "i") };
+    if (title) filter.title = new RegExp("^" + title, "i");
+    if (content) filter.content = new RegExp(content, "i");
     if (tags && tags.length) filter.tags = { $in: tags };
     if (origin) filter.origin = origin;
     if (cFlag !== undefined) filter.isComment = cFlag;
