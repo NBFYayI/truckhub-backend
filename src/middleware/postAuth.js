d0e6b1b93133a34209a19f94d769789c7a5ae442
Yoshisora <93548125+Yoshisora@users.noreply.github.com>
2024-05-22 21:42:22 -0700

    fk

--- src/middleware/postAuth.js
+++ src/middleware/postAuth.js
@@ -1,7 +1,26 @@
 const jwt = require("jsonwebtoken");
 const SECRET_KEY = require("../configs/secretKey");
+const { getPost } = require("./controllers/post-control");
 
 const postVerify = (req, res, next) => {
+  const token = req.cookies.token;
+
+  if (!token) {
+    return res.status(401).json({ message: "Unauthorized" });
+  }
+
+  try {
+    const decoded = jwt.verify(token, SECRET_KEY);
+    if (req.body.author !== decoded.username) {
+      throw new Error();
+    }
+    next();
+  } catch (err) {
+    return res.status(401).json({ message: "Invalid token" });
+  }
+};
+
+const authorVerify = (req, res, next) => {
   const token = req.cookies.token;
 
   if (!token) {
