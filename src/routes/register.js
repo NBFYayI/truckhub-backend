3261b2d7118c13143a5d2b8bcfd9cf527c753015
Yoshisora <93548125+Yoshisora@users.noreply.github.com>
2024-06-01 19:58:58 -0700

    fk

--- src/routes/register.js
+++ src/routes/register.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const { register } = require("../controllers/loginControl");
+const { register } = require("../controllers/login-control");
 
 router.post("/", async (req, res) => {
   try {
