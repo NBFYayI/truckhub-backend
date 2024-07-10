b110f68dd2d76ad03fc7f62301ecf456645b783a
SiwenTao <st5297@nyu.edu>
2024-07-10 19:06:43 -0400

    1231
    23

--- src/server.js
+++ src/server.js
@@ -9,7 +9,7 @@ const server = http.createServer(app);
 const { Server } = require("socket.io");
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:3000/", // The frontend's origin
+    origin: "http://localhost:3000", // The frontend's origin
     methods: ["GET", "POST"],
     credentials: true,
     allowedHeaders: ["Content-Type", "Authorization"],
